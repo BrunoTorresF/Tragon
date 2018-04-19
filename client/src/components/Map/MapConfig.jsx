8bfc7912f3e89b484cddce8e96b57d5775fd69ef
Bruno <bruno.torres.fabila@gmail.com>
2018-04-19 12:08:11 -0500

    refactor code

--- client/src/components/Map/MapConfig.jsx
+++ client/src/components/Map/MapConfig.jsx
@@ -10,22 +10,24 @@ const MyGoogleMap = compose(
   withProps({
     googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_API}`,
     loadingElement: <div style={{height: `100%`}} />,
-  containerElement:<div  style={{height: `240px`, width:`300px`,bottom:'100px'}}/>,
-    mapElement:<div style={{height:`100%`}}/>,
+    containerElement: <div style={{height: `240px`, width:`300px`,bottom:'100px'}} />,
+    mapElement: <div style={{height: `100%`}} />,
   }),
   withScriptjs,
-  withGoogleMap
-)((props) =>
+  withGoogleMap,
+)((props) => (
   <GoogleMap
-    defaultZoom = {11}
-    defaultCenter={{lat:props.loc.coordinates[0], lng:props.loc.coordinates[1]}}
+    defaultZoom={11}
+    defaultCenter={{lat: props.loc.coordinates[0], lng: props.loc.coordinates[1]}}
   >
-     <Marker
-            position={{lat:props.loc.coordinates[0],
-                       lng:props.loc.coordinates[1]}}
-            />
-</GoogleMap>
-)
+    <Marker
+      position={{
+        lat: props.loc.coordinates[0],
+        lng: props.loc.coordinates[1],
+      }}
+    />
+  </GoogleMap>
+));
 
 export default MyGoogleMap;
 // resize to fit with the result page with vendor card
